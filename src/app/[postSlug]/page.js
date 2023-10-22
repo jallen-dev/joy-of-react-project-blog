01ac74c972646fe2867f0932005ece306b6735ef
Justin Allen <jallen@jallen.dev>
2023-10-21 23:08:05 -0400

    Complete exercise 4

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -2,6 +2,7 @@ import React from "react";
 import { MDXRemote } from "next-mdx-remote/rsc";
 
 import BlogHero from "@/components/BlogHero";
+import CodeSnippet from "@/components/CodeSnippet";
 import { loadBlogPost } from "@/helpers/file-helpers";
 
 import styles from "./postSlug.module.css";
@@ -25,7 +26,10 @@ async function BlogPost({ params }) {
         publishedOn={blogPost.frontmatter.publishedOn}
       />
       <div className={styles.page}>
-        <MDXRemote source={blogPost.content} />
+        <MDXRemote
+          source={blogPost.content}
+          components={{ pre: CodeSnippet }}
+        />
       </div>
     </article>
   );
